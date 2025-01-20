c315809c8eab52af25ec8d67d1a45e90052a4b99
roshan1206 <sahuroshan96@gmail.com>
2025-01-20 18:22:08 +0530

    Added lambda expression

--- src/javacore/Anagram.java
+++ src/javacore/Anagram.java
@@ -1,12 +1,10 @@
 package javacore;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class UserMainCodeAnagram{
-	public static int getAnagram(String str1, String str2) {
+	public int getAnagram(String str1, String str2) {
 		ArrayList<String> splitStr1 = splitStringIntoArrayList(str1);
 		ArrayList<String> splitStr2 = splitStringIntoArrayList(str2);
 		ArrayList<String> newStrings = new ArrayList<String>();
@@ -29,11 +27,8 @@ class UserMainCodeAnagram{
 		
 	}
 	
-	public static ArrayList<String> splitStringIntoArrayList(String str) {
-		String[] splitStr = str.split("");
-		ArrayList<String> listStrings = new ArrayList<String>
-									(Arrays.asList(splitStr));
-		return listStrings;
+	public ArrayList<String> splitStringIntoArrayList(String str) {
+		return Arrays.stream(str.split("")).collect(Collectors.toCollection(ArrayList::new));
 	}
 }
 
@@ -41,21 +36,20 @@ public class Anagram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter 1st string : ");
-		String str1 = scanner.nextLine();
-		System.out.println("Enter 2nd string : ");
-		String str2 = scanner.nextLine();
-		scanner.close();
-//		String str1 = "eleven plus two";
-//		String str2 = "twelve plus one";
-		int result = UserMainCodeAnagram.getAnagram(str1, str2);
+		UserMainCodeAnagram userMainCodeAnagram = new UserMainCodeAnagram();
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Enter 1st string : ");
+//		String str1 = scanner.nextLine();
+//		System.out.println("Enter 2nd string : ");
+//		String str2 = scanner.nextLine();
+//		scanner.close();
+		String str1 = "eleven plus two";
+		String str2 = "twelve plus one";
+		int result = userMainCodeAnagram.getAnagram(str1, str2);
 		if(result==1) {
 			System.out.println("Anagrams");
 		}else {
 			System.out.println("Not Anagrams");
 		}
-
-	}
-
+    }
 }
