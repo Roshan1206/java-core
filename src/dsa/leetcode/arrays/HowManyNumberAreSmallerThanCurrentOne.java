5938fadf54cac94dfb7c172b733178eb079037d6
roshan1206 <sahuroshan96@gmail.com>
2025-02-25 09:45:23 +0530

    Added 1365 : How Many Number Are Smaller Than Current One with my sol and optimised sol

--- src/dsa/leetcode/arrays/HowManyNumberAreSmallerThanCurrentOne.java
+++ src/dsa/leetcode/arrays/HowManyNumberAreSmallerThanCurrentOne.java
@@ -29,8 +29,8 @@ public class HowManyNumberAreSmallerThanCurrentOne {
         int[] count = new int[101];
         int[] res = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            count[nums[i]]++;
+        for (int num : nums) {
+            count[num]++;
         }
 
         for (int i = 1; i <= 100; i++) {
