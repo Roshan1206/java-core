a36117423857e6960ba2b02b67e4ba36e9fe3ea1
roshan1206 <sahuroshan96@gmail.com>
2025-03-06 10:13:03 +0530

    binary search descc order logic added

--- src/dsa/search/BinarySearch.java
+++ src/dsa/search/BinarySearch.java
@@ -22,15 +22,28 @@ public class BinarySearch {
         int start = 0;
         int end = array.length - 1;
 
+        boolean isAsc = array[start] < array[end];
+
         while (start <= end){
 //          find the middle element. below one will work for large array size also
             int mid = start + (end-start)/2;
-            if(target < array[mid]){
-                end = mid-1;
-            } else if (target > array[mid]) {
-                start = mid + 1;
-            } else {
+
+            if(target == array[mid]){
                 return mid;
+            }
+
+            if(isAsc){
+                if(target < array[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid + 1;
+                }
+            } else{
+                if(target > array[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid + 1;
+                }
             }
         }
         return -1;
